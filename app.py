0e8ec93b7451e354dcb22cb07fed7402b9a0673a
SAMUEL AKINOLA ELUYEFA <149276078+MRKIKSY@users.noreply.github.com>
2026-01-16 17:54:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -72,3 +72,4 @@ SIGNED AND EXECUTED ELECTRONICALLY ON THIS {date}.
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+    
